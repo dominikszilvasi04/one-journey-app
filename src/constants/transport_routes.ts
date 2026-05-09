90f04e540f46a91e83b2ac1e449ea1e7f3e3f2f4
Dominik Szilvasi <dominik.szilvasi.2024@mumail.ie>
2026-05-09 19:59:27 +0100

    fix connolly hazelhatch line with correct irish rail station codes

--- src/constants/transport_routes.ts
+++ src/constants/transport_routes.ts
@@ -1,39 +1,173 @@
 export const LUAS_RED_LINE = [
-  ['TPT', 'SDK', 'MYS', 'GDK', 'CON', 'BUS', 'ABB', 'JER', 'FOU', 'SMI', 'MUS', 'HEU', 'JAM', 'FAT', 'RIA', 'SUI', 'GOL', 'DRI', 'BLA', 'BLU', 'KYL', 'RED', 'KIN', 'BEL', 'COO', 'HOS', 'TAL'],
-  ['BEL', 'FET', 'CVN', 'CIT', 'FOR', 'SAG']
+  [
+    "TPT",
+    "SDK",
+    "MYS",
+    "GDK",
+    "CON",
+    "BUS",
+    "ABB",
+    "JER",
+    "FOU",
+    "SMI",
+    "MUS",
+    "HEU",
+    "JAM",
+    "FAT",
+    "RIA",
+    "SUI",
+    "GOL",
+    "DRI",
+    "BLA",
+    "BLU",
+    "KYL",
+    "RED",
+    "KIN",
+    "BEL",
+    "COO",
+    "HOS",
+    "TAL",
+  ],
+  ["BEL", "FET", "CVN", "CIT", "FOR", "SAG"],
 ];
-
 
 export const LUAS_GREEN_LINE = [
-  ['BRO', 'CAB', 'PHI', 'GRA', 'BRD', 'DOM', 'PAR', 'OUP', 'OGP', 'MAR', 'WES', 'TRY', 'DAW', 'STS', 'HAR', 'CHA', 'RAN', 'BEE', 'COW', 'MIL', 'WIN', 'DUN', 'BAL', 'KIL', 'STI', 'SAN', 'CPK', 'GLE', 'GAL', 'LEO', 'BAW', 'CCK', 'LAU', 'CHE', 'BRI']
+  [
+    "BRO",
+    "CAB",
+    "PHI",
+    "GRA",
+    "BRD",
+    "DOM",
+    "PAR",
+    "OUP",
+    "OGP",
+    "MAR",
+    "WES",
+    "TRY",
+    "DAW",
+    "STS",
+    "HAR",
+    "CHA",
+    "RAN",
+    "BEE",
+    "COW",
+    "MIL",
+    "WIN",
+    "DUN",
+    "BAL",
+    "KIL",
+    "STI",
+    "SAN",
+    "CPK",
+    "GLE",
+    "GAL",
+    "LEO",
+    "BAW",
+    "CCK",
+    "LAU",
+    "CHE",
+    "BRI",
+  ],
 ];
-
 
 export const DART_LINE = [
-  ['HOWTH', 'SUTTN', 'BYSDE', 'HWTHJ'],
-  ['MHIDE', 'PMNCK', 'GRGRD', 'HWTHJ'],
-  ['HWTHJ', 'KBRCK', 'RAHNY', 'HTOWN', 'KLSTR', 'CTARF', 'CNLLY', 'TARA', 'PERSE', 'GCDK', 'LDWNE', 'SMONT', 'SIDNY', 'BTSTN', 'BROCK', 'SEAPT', 'SHILL', 'DLERY', 'SCOVE', 'GLGRY', 'DLKEY', 'KILNY', 'SKILL', 'BRAY', 'GSTNS']
+  ["HOWTH", "SUTTN", "BYSDE", "HWTHJ"],
+  ["MHIDE", "PMNCK", "GRGRD", "HWTHJ"],
+  [
+    "HWTHJ",
+    "KBRCK",
+    "RAHNY",
+    "HTOWN",
+    "KLSTR",
+    "CTARF",
+    "CNLLY",
+    "TARA",
+    "PERSE",
+    "GCDK",
+    "LDWNE",
+    "SMONT",
+    "SIDNY",
+    "BTSTN",
+    "BROCK",
+    "SEAPT",
+    "SHILL",
+    "DLERY",
+    "SCOVE",
+    "GLGRY",
+    "DLKEY",
+    "KILNY",
+    "SKILL",
+    "BRAY",
+    "GSTNS",
+  ],
 ];
-
 
 export const MAYNOOTH_LINE = [
-  ['MYNTH', 'LXLSA', 'LXCON', 'CLSLA', 'CMINE', 'CNOCK', 'PHNPK', 'ASHTN', 'PELTN', 'BBRDG', 'DCDRA', 'CNLLY', 'TARA', 'PERSE']
+  [
+    "MYNTH",
+    "LXLSA",
+    "LXCON",
+    "CLSLA",
+    "CMINE",
+    "CNOCK",
+    "PHNPK",
+    "ASHTN",
+    "PELTN",
+    "BBRDG",
+    "DCDRA",
+    "CNLLY",
+    "TARA",
+    "PERSE",
+  ],
 ];
-
 
 export const M3_PARKWAY_LINE = [
-  ['M3WAY', 'DBYNE', 'HAFLD', 'CLSLA', 'CMINE', 'CNOCK', 'PHNPK', 'ASHTN', 'PELTN', 'BBRDG', 'DCKLS']
+  [
+    "M3WAY",
+    "DBYNE",
+    "HAFLD",
+    "CLSLA",
+    "CMINE",
+    "CNOCK",
+    "PHNPK",
+    "ASHTN",
+    "PELTN",
+    "BBRDG",
+    "DCKLS",
+  ],
 ];
 
-
-export const DOCKLANDS_ROUTING_WAYPOINT = { latitude: 53.3582, longitude: -6.2486 };
-
+export const DOCKLANDS_ROUTING_WAYPOINT = {
+  latitude: 53.3582,
+  longitude: -6.2486,
+};
 
 export const NORTHERN_COMMUTER = [
-  ['DDALK', 'DGHDA', 'LTOWN', 'GSTON', 'BBRGN', 'SKRES', 'RLUSK', 'DBATE', 'MHIDE', 'PMNCK', 'GRGRD', 'HWTHJ', 'KBRCK', 'RAHNY', 'HTOWN', 'KLSTR', 'CTARF', 'CNLLY', 'TARA', 'PERSE']
+  [
+    "DDALK",
+    "DGHDA",
+    "LTOWN",
+    "GSTON",
+    "BBRGN",
+    "SKRES",
+    "RLUSK",
+    "DBATE",
+    "MHIDE",
+    "PMNCK",
+    "GRGRD",
+    "HWTHJ",
+    "KBRCK",
+    "RAHNY",
+    "HTOWN",
+    "KLSTR",
+    "CTARF",
+    "CNLLY",
+    "TARA",
+    "PERSE",
+  ],
 ];
 
-
 export const CONNOLLY_HAZELHATCH_LINE = [
-  ['CNLLY', 'HEU', 'PKWT', 'CLON', 'KISH', 'ADAM', 'HAZL']
+  ["CNLLY", "HSTON", "PWESS", "CLDKN", "KISHO", "ADAMS", "HZLCH"],
 ];
